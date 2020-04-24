ee463e8dfb99148221ba688688c30c01d0790eaa
sabujhasansarker <sabujhasansarker@gmail.com>
2020-04-24 13:19:31 +0600

    DB Connection & Create Router

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
