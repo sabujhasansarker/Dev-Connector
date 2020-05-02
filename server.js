b49f42b6675bce74638cb94688e08d810808a2ec
sabujhasansarker <sabujhasansarker@gmail.com>
2020-05-02 13:43:28 +0600

    Prepared for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 
+const path = require("path");
+
 const app = express();
 
 // Connect Database
@@ -15,6 +17,14 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
